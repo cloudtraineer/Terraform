040db4fd585ab880fd1d675d69ebe55b41b24c4e
cloudtraineer <awscourseonline@gmail.com>
2023-10-27 12:51:00 +0530

    new

---
+++ code/07-Extras/role/main.tf
@@ -0,0 +1,22 @@
+# Configure the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"
+  assume_role {
+    role_arn = "arn:aws:iam::<account>:role/<role_name>"
+    session_name = "myseesion"
+  }
+}
+
+
+# Create a S3 bucket
+resource "aws_s3_bucket" "aws_bucket" {
+  bucket		  = "firstbucket"
+}
